7ea5319e65107df911b67159c07387afe20bbde7
Alejandro Barrientos <alejoba1097@gmail.com>
2023-10-10 22:20:34 -0500

    Started terraform projects with simple EC2 Instance

---
+++ AWS/terraform/providers.tf
@@ -0,0 +1,11 @@
+# Use the following code in case AWS CLI is not installed
+# AWS CLI will use the config and credentials files in ~/.aws
+
+
+/*
+provider "aws" {
+    region = "us-east-1"
+    access_key = ""
+    secret_key = ""
+}
+*/
